1b757045024d2e7295af9520d07fa58562061464
renojackson922 <renojackson922@gmail.com>
2021-09-11 15:58:45 +0900

    비동기 테스트

--- src/main/java/com/silkdog/ambulance/AmbulanceApplication.java
+++ src/main/java/com/silkdog/ambulance/AmbulanceApplication.java
@@ -3,7 +3,6 @@ package com.silkdog.ambulance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 //@EnableAspectJAutoProxy
 @ServletComponentScan
